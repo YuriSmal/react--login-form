9d20a8ca6cdc9db646d60396d84564b6d21ce9c0
Yurii Smal <smalygga1488@gmail.comgit config --global user.name Yurii>
2020-10-31 21:22:09 +0200

    task 1 without Formik is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import InputSearch from './inputSearch/inputSearch';
-import ReadRandomNumber from './randomRead/randomRead'
-import List from './toDoList/toDoList'
 import LoginForm from './loginForm/loginForm'
 
 function App() {
   return ( 
     <div className='app-wrapper'>
-      {/* <InputSearch/>
-      <ReadRandomNumber/> */}
-      {/* <List/> */}
       <LoginForm/>
     </div>
     
